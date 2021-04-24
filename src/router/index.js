c2241d8837279210045a52da30fc857f14aada47
LinaDmi <AlinaDmi@users.noreply.github.com>
2021-04-24 15:01:04 +0300

    still working before auth

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,7 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+  { path: '/', name: 'Home', component: Home},
   {
     path: '/catalog',
     name: 'Catalog',
@@ -17,7 +13,8 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Catalog.vue')
-  }
+  },
+  { path: '*', redirect: '/' }
 ]
 
 const router = new VueRouter({
